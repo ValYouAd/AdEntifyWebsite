e3c418aa47bb3df60d3dd1bdf300ab249eef15cb
Pierrick Martos <pierrick@martos.me>
2013-04-18 16:29:38 +0200

    modification triggers

--- triggers.sql
+++ triggers.sql
@@ -1,12 +1,10 @@
 # Mise à jour du compteur de commentaires pour la photo qui vient d'être commentée
 CREATE TRIGGER comments_count AFTER INSERT ON comment
- FOR EACH ROW UPDATE photo SET comments_count = comments_count+1
-WHERE id = NEW.photo_id;
+ FOR EACH ROW UPDATE photo SET comments_count = comments_count+1 WHERE id = NEW.photo_id;
 
 # Mise à jour du compteur de likes pour la photo qui vient d'être liké
 CREATE TRIGGER likes_count AFTER INSERT ON `like`
- FOR EACH ROW UPDATE photo SET likes_count = likes_count+1
-WHERE id = NEW.photo_id;
+ FOR EACH ROW UPDATE photo SET likes_count = likes_count+1 WHERE id = NEW.photo_id;
 
 # Mise à jour compteurs de tags
 DELIMITER $$
@@ -14,6 +12,17 @@ CREATE TRIGGER tags_count AFTER INSERT ON `tag`
  FOR EACH ROW BEGIN
     UPDATE photo SET tags_count = tags_count+1 WHERE id = NEW.photo_id;
     UPDATE brand SET tags_count = tags_count+1 WHERE id = NEW.brand_id;
-    UPDATE venue SET tags_count = tags_count+1 WHERE id = NEW.purchaseVenue_id;
+    UPDATE venue SET tags_count = tags_count+1 WHERE id = NEW.venue_id;
+    UPDATE product SET tags_count = tags_count+1 WHERE id = NEW.product_id;
+    UPDATE person SET tags_count = tags_count+1 WHERE id = NEW.person_id;
+ END$$
+DELIMITER ;
+
+# Mise à jour des compteurs de produits
+DELIMITER $$
+CREATE TRIGGER products_count AFTER INSERT ON `product`
+ FOR EACH ROW BEGIN
+    UPDATE brand SET products_count = products_count+1 WHERE id = NEW.brand_id;
+    UPDATE venue SET products_count = products_count+1 WHERE id = NEW.purchaseVenue_id;
  END$$
 DELIMITER ;
