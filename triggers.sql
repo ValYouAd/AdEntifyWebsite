b016f3bb45b39905bc3a2326cb3ce84338b005f5
Pierrick Martos <pierrick@martos.me>
2013-04-18 13:27:34 +0200

    Mise à jour des triggers + corebundle

--- triggers.sql
+++ triggers.sql
@@ -1,14 +1,19 @@
 # Mise à jour du compteur de commentaires pour la photo qui vient d'être commentée
-CREATE TRIGGER photo_comments_count AFTER INSERT ON comment
+CREATE TRIGGER comments_count AFTER INSERT ON comment
  FOR EACH ROW UPDATE photo SET comments_count = comments_count+1
-WHERE id = NEW.photo_id
+WHERE id = NEW.photo_id;
 
 # Mise à jour du compteur de likes pour la photo qui vient d'être liké
-CREATE TRIGGER photo_likes_count AFTER INSERT ON `like`
+CREATE TRIGGER likes_count AFTER INSERT ON `like`
  FOR EACH ROW UPDATE photo SET likes_count = likes_count+1
-WHERE id = NEW.photo_id
+WHERE id = NEW.photo_id;
 
-# Mise à jour du compteur de tags pour la photo qui vient d'être tagué
-CREATE TRIGGER photo_tags_count AFTER INSERT ON `tag`
- FOR EACH ROW UPDATE photo SET tags_count = tags_count+1
-WHERE id = NEW.photo_id
+# Mise à jour compteurs de tags
+DELIMITER $$
+CREATE TRIGGER tags_count AFTER INSERT ON `tag`
+ FOR EACH ROW BEGIN
+    UPDATE photo SET tags_count = tags_count+1 WHERE id = NEW.photo_id;
+    UPDATE brand SET tags_count = tags_count+1 WHERE id = NEW.brand_id;
+    UPDATE venue SET tags_count = tags_count+1 WHERE id = NEW.purchaseVenue_id;
+ END$$
+DELIMITER ;
