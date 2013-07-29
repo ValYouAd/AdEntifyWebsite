993d93bf02e96c5f2e74670fd3a8bd6c39f0fb02
Pierrick Martos <pierrick@martos.me>
2013-07-30 00:39:23 +0200

    correction bug REST

---
+++ web/js/e470bcb_facebookConnect_1.js
@@ -0,0 +1,27 @@
+function facebookConnect() {
+   $('#authent-container').fadeOut('fast', function() {
+      $('#loading').fadeIn('fast');
+   });
+
+   FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+         window.location.href = Routing.generate('_security_check_facebook');
+      } else if (response.status === 'not_authorized') {
+         FB.login(function(response) {
+            if (response.authResponse) {
+               window.location.href = Routing.generate('_security_check_facebook');
+            } else {
+               // cancelled
+            }
+         }, { scope: facebookPermissions });
+      } else {
+         FB.login(function(response) {
+            if (response.authResponse) {
+               window.location.href = Routing.generate('_security_check_facebook');
+            } else {
+               // cancelled
+            }
+         }, { scope: facebookPermissions });
+      }
+   });
+}
