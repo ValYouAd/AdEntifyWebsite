6d8cb03b10a9c75f322dbe924edf307ac3a6a068
Pierrick Martos <pierrick@martos.me>
2013-08-07 09:32:01 +0200

    fix #57

--- src/AdEntify/UserBundle/Controller/DefaultController.php
+++ src/AdEntify/UserBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
