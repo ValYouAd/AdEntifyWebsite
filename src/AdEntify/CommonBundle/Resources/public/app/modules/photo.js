a2ff2e2485ac893fbc4bd898a1bd1261c3536c2c
Pierrick Martos <pierrick@martos.me>
2014-03-14 18:28:35 +0100

    fix #200

--- src/AdEntify/CommonBundle/Resources/public/app/modules/photo.js
+++ src/AdEntify/CommonBundle/Resources/public/app/modules/photo.js
@@ -84,7 +84,7 @@ define([
          if (query.length > 0)
             query = '?' + query.join('&');
 
-         return '&lt;iframe src="' + app.rootUrl +'iframe/photo-' + this.get('id') + '.html' + query + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' + this.get("large_width") + 'px; height:' + this.get("large_height") + 'px;" width="' + this.get("large_width") + '" height="' + this.get("large_height") + '" allowTransparency="true"&gt;&lt;/iframe&gt;';
+         return '&lt;iframe src="' + app.rootUrl +'iframe/photo-' + this.get('id') + '.html' + query + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" width="' + this.get("large_width") + '" height="' + this.get("large_height") + '" allowTransparency="true"&gt;&lt;/iframe&gt;';
       },
 
       getBrands: function() {
