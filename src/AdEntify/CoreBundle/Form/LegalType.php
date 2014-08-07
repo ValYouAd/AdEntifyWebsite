98d294cd5d1759cb05a6e7434754d6a2aebf3118
huas <huas@valyouad.com>
2014-08-07 19:27:09 +0200

    #22 Endpoint CGU

---
+++ src/AdEntify/CoreBundle/Form/LegalType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AdEntify\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LegalType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('termsOfUse')
+            ->add('privacy')
+            ->add('legalNotices')
+            ->add('language')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdEntify\CoreBundle\Entity\Legal'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'adentify_corebundle_legal';
+    }
+}
