c84d2c88b4cf2a5e9f4edd0ee0480969fcea9ce4
huas <huas@valyouad.com>
2014-08-29 18:34:53 +0200

    #278 back office product

---
+++ src/AdEntify/BackofficeBundle/Controller/ProductController.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace AdEntify\BackofficeBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AdEntify\CoreBundle\Entity\Product;
+use AdEntify\CoreBundle\Form\ProductType;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+
+/**
+ * Product controller.
+ *
+ * @Route("/product")
+ */
+class ProductController extends Controller
+{
+    const PAGE_LIMIT = 10;
+
+    /**
+     * Lists all Product entities.
+     *
+     * @Route("/{page}", requirements={"page" = "\d+"}, defaults={"page" = 1}, name="product")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction($page = 1)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT product FROM AdEntify\CoreBundle\Entity\Product product')
+            ->setFirstResult(($page - 1) * self::PAGE_LIMIT)
+            ->setMaxResults(self::PAGE_LIMIT);
+
+        $paginator = new Paginator($query, true);
+
+        $c = count($paginator);
+
+        $delete_forms = array();
+
+        foreach ($paginator as $entity) {
+            $delete_forms[$entity->getId()] = $this->createDeleteForm($entity->getId())->createView();
+        }
+
+        return array(
+            'entities' => $paginator,
+            'count' => $c,
+            'pageLimit' => self::PAGE_LIMIT,
+            'delete_forms' => $delete_forms,
+        );
+    }
+    /**
+     * Creates a new Product entity.
+     *
+     * @Route("/", name="product_create")
+     * @Method("POST")
+     * @Template("AdEntifyBackofficeBundle:Product:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Product();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('product_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to create a Product entity.
+     *
+     * @param Product $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Product $entity)
+    {
+        $form = $this->createForm(new ProductType(), $entity, array(
+            'action' => $this->generateUrl('product_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Product entity.
+     *
+     * @Route("/new", name="product_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Product();
+        $form   = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Product entity.
+     *
+     * @Route("/{id}", name="product_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AdEntifyCoreBundle:Product')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Product entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Product entity.
+     *
+     * @Route("/{id}/edit", name="product_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AdEntifyCoreBundle:Product')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Product entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+    * Creates a form to edit a Product entity.
+    *
+    * @param Product $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Product $entity)
+    {
+        $form = $this->createForm(new ProductType(), $entity, array(
+            'action' => $this->generateUrl('product_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Product entity.
+     *
+     * @Route("/{id}", name="product_update")
+     * @Method("PUT")
+     * @Template("AdEntifyBackofficeBundle:Product:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AdEntifyCoreBundle:Product')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Product entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('product_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Product entity.
+     *
+     * @Route("/{id}", name="product_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AdEntifyCoreBundle:Product')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Product entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('product'));
+    }
+
+    /**
+     * Creates a form to delete a Product entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete', 'attr' => array('class' => 'btn btn-danger')))
+            ->getForm()
+        ;
+    }
+}
