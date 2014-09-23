957fb7ef3bab61b865433a2442dd409fe3b98afd
huas <huas@valyouad.com>
2014-09-23 12:50:06 +0200

    test

--- src/AdEntify/CoreBundle/Tests/Controller/UserControllerTest.php
+++ src/AdEntify/CoreBundle/Tests/Controller/UserControllerTest.php
@@ -26,92 +26,56 @@ class UserControllerTest extends EnhancedWebTestCase
         $client->getCookieJar()->set($cookie);
     }
 
-    public function testGet()
+    public function assertAllGet($endpoint, $key, $authorization, $http_code = 200)
     {
-        // Create a new client to browse the application
-        $client = $this->getClient();
+        $client = $this->getCLient();
 
-        // Create a new entry in the database
-        $client->request('GET', sprintf('/api/v1/users/%s', self::USER_ID));
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /api/v1/users/1");
+        $http_authorization = ($authorization) ? array('HTTP_Authorization' => $this->getAuthorizationHeader()) : array();
 
-        $this->assertJsonResponse($client->getResponse(), 200);
+        $client->request('GET', $endpoint, array(), array(), $http_authorization);
+        $this->assertEquals($http_code, $client->getResponse()->getStatusCode(), sprintf("Unexpected HTTP status code for GET %s", $endpoint));
+        $this->assertJsonResponse($client->getResponse(), $http_code);
 
         $content = $client->getResponse()->getContent();
 
-        $decoded = json_decode($content, true);
-        $this->assertIsset($decoded, 'id');
+        if ($http_code != 400)
+        {
+            $decoded = json_decode($content, true);
+            $this->assertIsset($decoded, $key);
+        }
+    }
+
+    public function testGet()
+    {
+        $this->assertAllGet(sprintf('/api/v1/users/%s', self::USER_ID), 'id', false);
     }
 
     public function testGetCurrent()
     {
-        $client = $this->getClient();
-
-        $client->request('GET', '/api/v1/user/current', array(), array(), array(
-            'HTTP_Authorization' => $this->getAuthorizationHeader()
-        ));
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /api/v1/user/current");
-        $this->assertJsonResponse($client->getResponse(), 200);
-
-        $content = $client->getResponse()->getContent();
-
-        $decoded = json_decode($content, true);
-        $this->assertIsset($decoded, 'id');
+        $this->assertAllGet(sprintf('/api/v1/user/current'), 'id', true);
     }
 
     public function testGetPhotos()
     {
-        // Create a new client to browse the application
-        $client = $this->getClient();
-        $this->login($client);
-
-        // Create a new entry in the database
-        $client->request('GET', sprintf('/api/v1/users/%s/photos', self::USER_ID));
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), sprintf('Unexpected HTTP status code for GET /api/v1/users/%s/photos', self::USER_ID));
-        $this->assertJsonResponse($client->getResponse(), 200);
-
-        $content = $client->getResponse()->getContent();
-
-        $decoded = json_decode($content, true);
-        $this->assertIsset($decoded, 'data');
+        $this->assertAllGet(sprintf('/api/v1/users/%s/photos', self::USER_ID), 'data', false);
     }
 
     public function testGetFavorites()
     {
-        $client = $this->getCLient();
-
-        $client->request('GET', '/api/v1/user/favorites', array(), array(), array('HTTP_Authorization' => $this->getAuthorizationHeader()));
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /api/v1/user/favorites');
-        $this->assertJsonResponse($client->getResponse(), 200);
-
-        $content = $client->getResponse()->getContent();
-
-        $decoded = json_decode($content, true);
-        $this->assertIsset($decoded, 'data');
+        $this->assertAllGet('/api/v1/user/favorites', 'data', true);
     }
 
     public function testGetSearch()
     {
-        $http_method = 'GET';
-        $endpoint = '/api/v1/user/search';
         $query_test = ["", "h", "pi", "8", "42", "ak47"];
-
-        $client = $this->getCLient();
-
         foreach($query_test as $query)
         {
             $http_code = ($query == "") ? 400 : 200;
-            $client->request($http_method, sprintf($endpoint.'?query=%s', $query), array(), array(), array('HTTP_Authorization' => $this->getAuthorizationHeader()));
-            $this->assertEquals($http_code, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for '.$http_method.' '.$endpoint.' with the query '.$query.' .');
-            $this->assertJsonResponse($client->getResponse(), $http_code);
-
-            if ($query != "") {
-                $content = $client->getResponse()->getContent();
-
-                $decoded = json_decode($content, true);
-                $this->assertIsset($decoded, 'data');
-            }
+            $this->assertAllGet(sprintf('/api/v1/user/search?query=%s', $query), 'data', true, $http_code);
         }
+    }
+    public function testGetIsFollowed()
+    {
+        $this->assertAllGet('/api/v1/user/favorites', 'data', true);
     }
 }
