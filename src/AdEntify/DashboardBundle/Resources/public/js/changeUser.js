ed23274e91527af4ea6026fcf4a3f609f000f1e4
huas <huas@valyouad.com>
2014-12-23 15:20:20 +0100

    fix location href

--- src/AdEntify/DashboardBundle/Resources/public/js/changeUser.js
+++ src/AdEntify/DashboardBundle/Resources/public/js/changeUser.js
@@ -1,10 +1,11 @@
 /**
  * Created by pierrickmartos on 23/12/14.
  */
-$(document).ready(function() {
+jQuery(document).ready(function() {
    $('.change-account-btn').click(function() {
       if ($('.accounts-selector').val()) {
          window.location.href = $('.accounts-selector').val();
       }
+      return false;
    });
 });
